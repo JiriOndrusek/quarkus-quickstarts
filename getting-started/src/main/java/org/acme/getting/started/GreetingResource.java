f85efd2f2a6cac422065b19f6d43d5a309244f7b
JiriOndrusek <ondrusek.jiri@gmail.com>
2022-07-11 12:56:20 +0200

    Simple bug reproducer

--- getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
+++ getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
@@ -1,5 +1,7 @@
 package org.acme.getting.started;
 
+import org.jboss.logging.Logger;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,6 +11,9 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
+
+
     @Inject
     GreetingService service;
 
@@ -16,12 +21,14 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/greeting/{name}")
     public String greeting(String name) {
+        LOG.info("Greeting");
         return service.greeting(name);
     }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOG.info("Hello");
         return "hello";
     }
 }
